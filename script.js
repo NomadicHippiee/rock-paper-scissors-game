715932e91a5d9611b441555fbd2bbed47ecfdf51
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2025-11-29 00:31:50 +0100

    Made function for playRound

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
         return "rock"
-    } else if (randomNumber === 2) {
+    } else if (computerChoice === 2) {
         return "paper"
     } else {
         return "scissors"
     }
 }
-console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper or Scissors?")
+    return humanChoice.toLowerCase();
+
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Equal! Both win")
+    } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You Win!")
+    } else {
+        console.log("Computer Won...")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+
+console.log(playRound(humanSelection, computerSelection))
+console.log(computerSelection)
+
+
+
+
